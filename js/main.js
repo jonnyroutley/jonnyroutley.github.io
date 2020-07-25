18b1cf6ab4fef3ca466e7592190ba2bd859510f6
Jonathan Routley <jonathan.wei.liang@gmail.com>
2020-07-25 23:07:49 +0100

    rebuilding site Sat, Jul 25, 2020 11:07:49 PM

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
 
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function() {stickyNavbar()};
+// // When the user scrolls the page, execute myFunction 
+// window.onscroll = function() {stickyNavbar()};
 
-// Get the navbar
-var navbar = document.getElementById("navbar");
+// // Get the navbar
+// var navbar = document.getElementById("navbar");
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+// // Get the offset position of the navbar
+// var sticky = navbar.offsetTop;
 
-var mainPage = document.getElementById("main_page")
+// var mainPage = document.getElementById("main_page")
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function stickyNavbar() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky");
-        mainPage.classList.add("buffer");
-    } 
-    else {
-        navbar.classList.remove("sticky");
-        mainPage.classList.remove("buffer");
-    }
-}
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function stickyNavbar() {
+//     if (window.pageYOffset >= sticky) {
+//         navbar.classList.add("sticky");
+//         mainPage.classList.add("buffer");
+//     } 
+//     else {
+//         navbar.classList.remove("sticky");
+//         mainPage.classList.remove("buffer");
+//     }
+// }
 
